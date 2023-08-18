6759a0568e9f1efce02bee271fbdd502753fb877
toka <tokazerkje@outlook.com>
2023-08-18 15:22:26 +0200

    add docker

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker build . -t image_libfuzzer
+docker build . -t image_libafl_libfuzzer
 
 for i in {0..7}
 do
-  docker run -t --name image_libfuzzer-"$i" -d image_libfuzzer "/work/image/fuzz/fuzz.sh" "libfuzzer" $i
+  docker run -t --name image_libafl_libfuzzer-"$i" -d image_libafl_libfuzzer "/work/image/fuzz/fuzz.sh" "libafl_libfuzzer" $i
 done
 
 for i in {0..7}
 do
-  docker wait image_libfuzzer-"$i"
+  docker wait image_libafl_libfuzzer-"$i"
 done
